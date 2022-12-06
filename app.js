626c8d8bfb0af13b78da2c9e41a2a030bbfc903f
charleslamb212 <charlesandrewlamb@gmail.com>
2022-12-06 15:32:15 -0800

    changed button

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ const squares = document.querySelectorAll(".board div");
 let result = document.querySelector(".result");
 let time = document.querySelector(".time");
 let timer = 50;
+//establish lengthof game time
 let currentIndex = 76;
+//currentIndex is the starting div (76th div)
 let width = 9;
-
+//game board width is 9 squares
+//DOM elements
 let leftLogs = document.querySelectorAll(".left-log");
 let rightLogs = document.querySelectorAll(".right-log");
 let leftCar = document.querySelectorAll(".left-car");
 let rightCar = document.querySelectorAll(".right-car");
-let endingBlock = document.querySelector(".ending-block");
+let endingDiv = document.querySelector(".end");
 //assign functionality to the arrow keys
 function moveFrogger(e) {
   squares[currentIndex].classList.remove("frogger");
@@ -45,80 +48,80 @@ function move() {
   rightCar.forEach((rightCar) => moveRightCar(rightCar));
   checkWin();
 }
-
+// 
 function moveLeftLog(leftLog) {
-  if (leftLog.classList.contains("l1")) {
-    leftLog.classList.remove("l1");
-    leftLog.classList.add("l2");
-  } else if (leftLog.classList.contains("l2")) {
-    leftLog.classList.remove("l2");
-    leftLog.classList.add("l3");
-  } else if (leftLog.classList.contains("l3")) {
-    leftLog.classList.remove("l3");
-    leftLog.classList.add("l4");
-  } else if (leftLog.classList.contains("l4")) {
-    leftLog.classList.remove("l4");
-    leftLog.classList.add("l5");
-  } else if (leftLog.classList.contains("l5")) {
-    leftLog.classList.remove("l5");
-    leftLog.classList.add("l1");
+  if (leftLog.classList.contains("a1")) {
+    leftLog.classList.remove("a1");
+    leftLog.classList.add("a2");
+  } else if (leftLog.classList.contains("a2")) {
+    leftLog.classList.remove("a2");
+    leftLog.classList.add("a3");
+  } else if (leftLog.classList.contains("a3")) {
+    leftLog.classList.remove("a3");
+    leftLog.classList.add("a4");
+  } else if (leftLog.classList.contains("a4")) {
+    leftLog.classList.remove("a4");
+    leftLog.classList.add("a5");
+  } else if (leftLog.classList.contains("a5")) {
+    leftLog.classList.remove("a5");
+    leftLog.classList.add("a1");
   }
 }
 
 function moveRightLog(rightLog) {
-  if (rightLog.classList.contains("l1")) {
-    rightLog.classList.remove("l1");
-    rightLog.classList.add("l5");
-  } else if (rightLog.classList.contains("l2")) {
-    rightLog.classList.remove("l2");
-    rightLog.classList.add("l1");
-  } else if (rightLog.classList.contains("l3")) {
-    rightLog.classList.remove("l3");
-    rightLog.classList.add("l2");
-  } else if (rightLog.classList.contains("l4")) {
-    rightLog.classList.remove("l4");
-    rightLog.classList.add("l3");
-  } else if (rightLog.classList.contains("l5")) {
-    rightLog.classList.remove("l5");
-    rightLog.classList.add("l4");
+  if (rightLog.classList.contains("a1")) {
+    rightLog.classList.remove("a1");
+    rightLog.classList.add("a5");
+  } else if (rightLog.classList.contains("a2")) {
+    rightLog.classList.remove("a2");
+    rightLog.classList.add("a1");
+  } else if (rightLog.classList.contains("a3")) {
+    rightLog.classList.remove("a3");
+    rightLog.classList.add("a2");
+  } else if (rightLog.classList.contains("a4")) {
+    rightLog.classList.remove("a4");
+    rightLog.classList.add("a3");
+  } else if (rightLog.classList.contains("a5")) {
+    rightLog.classList.remove("a5");
+    rightLog.classList.add("a4");
   }
 }
 
 function moveLeftCar(leftCar) {
-  if (leftCar.classList.contains("c1")) {
-    leftCar.classList.remove("c1");
-    leftCar.classList.add("c2");
-  } else if (leftCar.classList.contains("c2")) {
-    leftCar.classList.remove("c2");
-    leftCar.classList.add("c3");
-  } else if (leftCar.classList.contains("c3")) {
-    leftCar.classList.remove("c3");
-    leftCar.classList.add("c1");
-  } else if (leftCar.classList.contains("c1")) {
-    leftCar.classList.remove("c41");
-    leftCar.classList.add("c2");
-  } else if (leftCar.classList.contains("c2")) {
-    leftCar.classList.remove("c2");
-    leftCar.classList.add("c3");
+  if (leftCar.classList.contains("b1")) {
+    leftCar.classList.remove("b1");
+    leftCar.classList.add("b2");
+  } else if (leftCar.classList.contains("b2")) {
+    leftCar.classList.remove("b2");
+    leftCar.classList.add("b3");
+  } else if (leftCar.classList.contains("b3")) {
+    leftCar.classList.remove("b3");
+    leftCar.classList.add("b1");
+  } else if (leftCar.classList.contains("b1")) {
+    leftCar.classList.remove("b1");
+    leftCar.classList.add("b2");
+  } else if (leftCar.classList.contains("b2")) {
+    leftCar.classList.remove("b2");
+    leftCar.classList.add("b3");
   }
 }
 
 function moveRightCar(rightCar) {
-  if (rightCar.classList.contains("c1")) {
-    rightCar.classList.remove("c1");
-    rightCar.classList.add("c3");
-  } else if (rightCar.classList.contains("c2")) {
-    rightCar.classList.remove("c2");
-    rightCar.classList.add("c1");
-  } else if (rightCar.classList.contains("c3")) {
-    rightCar.classList.remove("c3");
-    rightCar.classList.add("c2");
-  } else if (rightCar.classList.contains("c1")) {
-    rightCar.classList.remove("c1");
-    rightCar.classList.add("c3");
-  } else if (rightCar.classList.contains("c2")) {
-    rightCar.classList.remove("c2");
-    rightCar.classList.add("c1");
+  if (rightCar.classList.contains("b1")) {
+    rightCar.classList.remove("b1");
+    rightCar.classList.add("b3");
+  } else if (rightCar.classList.contains("b2")) {
+    rightCar.classList.remove("b2");
+    rightCar.classList.add("b1");
+  } else if (rightCar.classList.contains("b3")) {
+    rightCar.classList.remove("b3");
+    rightCar.classList.add("b2");
+  } else if (rightCar.classList.contains("b1")) {
+    rightCar.classList.remove("b1");
+    rightCar.classList.add("b3");
+  } else if (rightCar.classList.contains("b2")) {
+    rightCar.classList.remove("b2");
+    rightCar.classList.add("b1");
   }
 }
 //piping in the if statement checks if anyof these conditions are true, and if so, triggers the timer to stop and thus ending the game.
@@ -126,10 +129,10 @@ function gameOver() {
   timer--;
   time.textContent = timer;
   if (
-    squares[currentIndex].classList.contains("l4") ||
-    squares[currentIndex].classList.contains("l5") ||
-    squares[currentIndex].classList.contains("c1") ||
-    squares[currentIndex].classList.contains("l3") ||
+    squares[currentIndex].classList.contains("a4") ||
+    squares[currentIndex].classList.contains("a5") ||
+    squares[currentIndex].classList.contains("b1") ||
+    squares[currentIndex].classList.contains("a3") ||
     timer === 0
   ) {
     //alert('gameOver')
@@ -138,7 +141,7 @@ function gameOver() {
     clearInterval(timerId);
   }
 }
-let moving = setInterval(move, 1000);
+let moving = setInterval(move, 800);
 
 function winOrLose() {
   gameOver();
@@ -164,8 +167,8 @@ document.querySelector(".btn").addEventListener('click', e => {
   reset()
 })
 function checkWin() {
-  if (endingBlock.classList.contains("frogger")) {
-    // if(squares[currentIndex].classList.contains('ending-block')) {
+  if (endingDiv.classList.contains("frogger")) {
+    // if(squares[currentIndex].classList.contains('endingDiv')) {
     //alert('You Won!!')
     console.log("You win");
     result.textContent = "You Win!!!";
